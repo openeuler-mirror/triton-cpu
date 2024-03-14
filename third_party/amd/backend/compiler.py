triton-lang/triton

commit 248d1fb07102cbb8721898c7b84273cc177e2a41

[FRONTEND] make compile-only work on non-gpu hosts (#3362)
Previously, we could compile Triton kernels on non-gpu hosts. However,
it started to fail recently once we switch to the latest version of
Triton.

In our workload, we have a way to compile all relevant Triton kernels
beforehand (AOT), and then launch/run these kernels on gpu hosts, so
kind of build and run are two separate processes.

This PR is trying to make Triton compile work again on non-gpu hosts.

third_party/amd/backend/compiler.py
@@ -89,6 +89,7 @@ class HIPBackend(BaseBackend):
         super().__init__(target)
         assert isinstance(target, tuple) and len(target) == 3
         assert isinstance(target[1], str)
+        self.binary_ext = "hsaco"
 
     def parse_options(self, opts) -> Any:
         args = {'arch': self.target[1]}

third_party/amd/backend/driver.py
@@ -260,7 +260,6 @@ class HIPDriver(GPUDriver):
     def __init__(self):
         super().__init__()
         self.utils = HIPUtils()
-        self.binary_ext = "hsaco"
         self.launcher_cls = HIPLauncher
 
     @staticmethod

python/triton/compiler/compiler.py
@@ -306,18 +306,18 @@ class CompiledKernel:
         self.metadata = json.loads(metadata_path.read_text())
         KernelMetadata = namedtuple('KernelMetadata', sorted(list(self.metadata.keys())))
         self.metadata = KernelMetadata(**self.metadata)
+        self.src = src
         self.hash = hash
 
         self.name = self.metadata.name
-        # create launcher
-        self.run = driver.active.launcher_cls(src, self.metadata)
         # stores the text of each level of IR that was generated during compilation
         asm_files = [Path(p) for c, p in metadata_group.items() if not c.endswith(".json")]
+        binary_ext = make_backend(self.metadata.target).binary_ext
         self.asm = {
-            file.suffix[1:]: file.read_bytes() if file.suffix[1:] == driver.active.binary_ext else file.read_text()
+            file.suffix[1:]: file.read_bytes() if file.suffix[1:] == binary_ext else file.read_text()
             for file in asm_files
         }
-        self.kernel = self.asm[driver.active.binary_ext]
+        self.kernel = self.asm[binary_ext]
         # binaries are lazily initialized
         # because it involves doing runtime things
         # (e.g., checking amount of shared memory on current device)
@@ -328,6 +328,8 @@ class CompiledKernel:
         if self.module is not None:
             return
         device = driver.active.get_current_device()
+        # create launcher
+        self.run = driver.active.launcher_cls(self.src, self.metadata)
         # not enough shared memory to run the kernel
         max_shared = driver.active.utils.get_device_properties(device)["max_shared_mem"]
         if self.metadata.shared > max_shared:

third_party/nvidia/backend/compiler.py
@@ -98,6 +98,7 @@ class CUDABackend(BaseBackend):
         super().__init__(target)
         self.capability = target[1]
         assert isinstance(self.capability, int)
+        self.binary_ext = "cubin"
 
     def parse_options(self, opts) -> Any:
         args = {k: opts[k] for k in CUDAOptions.__dataclass_fields__.keys() if k in opts}

third_party/nvidia/backend/driver.py
@@ -348,7 +348,6 @@ class CudaDriver(GPUDriver):
 
     def __init__(self):
         self.utils = CudaUtils()  # TODO: make static
-        self.binary_ext = "cubin"
         self.launcher_cls = CudaLauncher
         super().__init__()
 

openeuler-mirror/triton-cpu

commit 56089bb87700a4c6b03ac067e1992e0dc12ee97a

[AMD] Make target a list instead of a tuple for HIP backend (#3372)
Fix AMD backend failure caused by
https://github.com/openai/triton/pull/3362 and
https://github.com/openai/triton/pull/3370

third_party/amd/backend/compiler.py
@@ -82,12 +82,12 @@ class HIPOptions:
 class HIPBackend(BaseBackend):
 
     @staticmethod
-    def supports_target(target: tuple):
+    def supports_target(target: list):
         return target[0] == 'hip'
 
-    def __init__(self, target: tuple) -> None:
+    def __init__(self, target: list) -> None:
         super().__init__(target)
-        assert isinstance(target, tuple) and len(target) == 3
+        assert isinstance(target, list) and len(target) == 3
         assert isinstance(target[1], str)
         self.binary_ext = "hsaco"
 

third_party/amd/backend/driver.py
@@ -272,4 +272,4 @@ class HIPDriver(GPUDriver):
         device_properties = self.utils.get_device_properties(device)
         arch = device_properties['arch']
         warpSize = device_properties['warpSize']
-        return ("hip", arch.split(':')[0], warpSize)
+        return ["hip", arch.split(':')[0], warpSize]

python/test/unit/language/test_core.py
@@ -2793,6 +2793,8 @@ def test_dot(M, N, K, num_warps, col_a, col_b, epilogue, allow_tf32, in_dtype, o
             pytest.skip("Only test out_dtype=float16 on devices with sm >=80")
     if capability[0] < 9 and in_dtype == 'float8e4nv':
         pytest.skip("float8e4nv not supported on sm <= 80")
+    if is_hip() and (in_dtype == 'float8e4nv' or in_dtype == 'float8e5'):
+        pytest.skip("float8e4nv and float8e5 not supported on HIP")
     if is_interpreter() and in_dtype == 'int8':
         pytest.skip(
             "numpy.dot with int8 inputs will overflow while tl.dot doesn't because MMA instruction's accumulator is 32-bit"