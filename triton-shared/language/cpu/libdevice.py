0f924126e59670d495ba3acef1cb233ba0fc9e8c
workwilson <ogbonnachibuoyim12@gmail.com>
2026-03-30 15:56:45 +0800

    extend tensor conversion to other call sites with handle attribute

--- triton-shared/language/cpu/libdevice.py
+++ triton-shared/language/cpu/libdevice.py
@@ -4,128 +4,157 @@ from triton.language.core import builtin
 from triton import jit
 
 
+def _to_tensor(arg, _builder):
+    return core.to_tensor(arg, _builder=_builder)
+
+
 @core.extern
 def acos(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_acos(arg0.handle), arg0.type)
 
 
 @core.extern
 def acosh(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_acosh(arg0.handle), arg0.type)
 
 
 @core.extern
 def asin(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_asin(arg0.handle), arg0.type)
 
 
 @core.extern
 def asinh(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_asinh(arg0.handle), arg0.type)
 
 
 @core.extern
 def atan(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_atan(arg0.handle), arg0.type)
 
 
 @core.extern
 def atanh(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_atanh(arg0.handle), arg0.type)
 
 
 @core.extern
 def cbrt(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_cbrt(arg0.handle), arg0.type)
 
 
 @core.extern
 def cos(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_cos(arg0.handle), arg0.type)
 
 
 @core.extern
 def cosh(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_cosh(arg0.handle), arg0.type)
 
 
 @core.extern
 def erf(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_erf(arg0.handle), arg0.type)
 
 
 @core.extern
 def exp(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_exp(arg0.handle), arg0.type)
 
 
 @core.extern
 def exp2(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_exp2(arg0.handle), arg0.type)
 
 
 @core.extern
 def expm1(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_expm1(arg0.handle), arg0.type)
 
 
 @core.extern
 def floor(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_floor(arg0.handle), arg0.type)
 
 
 @core.extern
 def log(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_log(arg0.handle), arg0.type)
 
 
 @core.extern
 def log2(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_log2(arg0.handle), arg0.type)
 
 
 @core.extern
 def log10(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_log10(arg0.handle), arg0.type)
 
 
 @core.extern
 def log1p(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_log1p(arg0.handle), arg0.type)
 
 
 @core.extern
 def sin(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_sin(arg0.handle), arg0.type)
 
 
 @core.extern
 def rsqrt(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_rsqrt(arg0.handle), arg0.type)
 
 
 @core.extern
 def sqrt(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_sqrt(arg0.handle), arg0.type)
 
 
 @core.extern
 def sinh(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_sinh(arg0.handle), arg0.type)
 
 
 @core.extern
 def tan(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_tan(arg0.handle), arg0.type)
 
 
 @core.extern
 def tanh(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_tanh(arg0.handle), arg0.type)
 
 
 @core.extern
 def trunc(arg0, _builder=None):
+    arg0 = _to_tensor(arg0, _builder)
     return core.tensor(_builder.create_trunc(arg0.handle), arg0.type)
 
 
@@ -158,16 +187,16 @@ def fmod(arg0, arg1, _builder=None):
 
 @core.extern
 def div_rn(arg0, arg1, _builder=None):
-    arg0 = core.to_tensor(arg0, _builder=_builder)
-    arg1 = core.to_tensor(arg1, _builder=_builder)
+    arg0 = _to_tensor(arg0, _builder)
+    arg1 = _to_tensor(arg1, _builder)
     arg0, arg1 = core.binary_op_type_legalization(arg0, arg1, _builder)
     return core.tensor(_builder.create_precise_divf(arg0.handle, arg1.handle), arg0.type)
 
 
 @core.extern
 def div_rz(arg0, arg1, _builder=None):
-    arg0 = core.to_tensor(arg0, _builder=_builder)
-    arg1 = core.to_tensor(arg1, _builder=_builder)
+    arg0 = _to_tensor(arg0, _builder)
+    arg1 = _to_tensor(arg1, _builder)
     arg0, arg1 = core.binary_op_type_legalization(arg0, arg1, _builder)
     rn = core.tensor(_builder.create_precise_divf(arg0.handle, arg1.handle), arg0.type)
     return core.tensor(_builder.create_trunc(rn.handle), rn.type)
